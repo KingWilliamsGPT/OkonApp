47dbe5248160225577237a917216ef49318fecd3
KingWilliamsGPT <williamusanga22@gmail.com>
2026-02-22 20:24:29 +0100

    final stuff

--- app/src/main/java/com/example/okonapp/LoginActivity.java
+++ app/src/main/java/com/example/okonapp/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.okonapp;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.okonapp.databinding.ActivityLoginBinding; // Note: Assuming standard ViewBinding but I'll use findViewById for simplicity if not enabled.
 
 public class LoginActivity extends AppCompatActivity {
     @Override
